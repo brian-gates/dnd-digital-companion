023a175ee92f52bbc752d4ba2fcc6a2bfbef5e89
Brian Gates <brian.g.gates@gmail.com>
2018-06-02 14:42:07 -0700

    It logs characters
    enter the apiKey and base id, hit the button, watch the magic

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -2,22 +2,42 @@ import * as React from "react";
 import Button from "@material-ui/core/Button";
 import Input from "@material-ui/core/Input";
 import { log } from "util";
+import Airtable from "airtable";
+
 
 class HomePage extends React.Component<{}, {}> {
 
-    private keyChange(event: { target: { value: string; }; }) {
-        log(event.target.value)
-    } 
+    private eventTargetToStateKey(key: string) {
+        return (event: { target: { value: string; }; }) {
+            this.setState({ [key]: event.target.value })
+        }
+    }
+
+    private loadCharacters() {
+        const { apiKey, base } = this.state;
+        log({ apiKey, base })
+        const base = new Airtable({ apiKey }).base(base);
+        base('Characters').select({
+            sort: [
+                { field: 'Name', direction: 'asc' }
+            ]
+        }).eachPage(function page(records, fetchNextPage) {
+            records.forEach(function (record) {
+                console.log('Retrieved ', record.get('Name'));
+            });
+            fetchNextPage();
+        }, function done(error: Error) {
+            console.log(error);
+        });
+    }
 
     public render(): JSX.Element {
         return (
             <>
-                <Input
-                    type="password"
-                    onChange={this.keyChange}
-                />
-                <Button variant="raised" color="primary">
-                    Hello World
+                <Input onChange={this.eventTargetToStateKey('apiKey')} />
+                <Input onChange={this.eventTargetToStateKey('base')} />
+                <Button variant="raised" color="primary" onClick={() => this.loadCharacters()}>
+                    Load Characters
                 </Button>
             </>
         );
